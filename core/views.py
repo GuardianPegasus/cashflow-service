85f02217ce8edd24f1ddac1e571378734d2a3595
GuardianPegasus <napoleon.bona1811@gmail.com>
2026-07-02 19:45:08 +0700

    index update

--- core/views.py
+++ core/views.py
@@ -1,14 +1,45 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
+from django.db.models import Q
+from django.core.paginator import Paginator
 from .models import CashFlowRecord, Category, Subcategory, Status, Type
 from .forms import CashFlowRecordForm, StatusForm, TypeForm, CategoryForm, SubcategoryForm
 
 def index(request):
-    """Главная страница: вывод записей и обработка формы"""
-    
-    # Используем select_related для предотвращения проблемы запросов N+1
-    records = CashFlowRecord.objects.select_related('status', 'income_expense_type', 'category', 'subcategory').all()
-    
+    """Главная страница: вывод записей с фильтрацией, пагинацией и обработка формы"""
+    # Базовый QuerySet с оптимизацией запросов
+    records_list = CashFlowRecord.objects.select_related(
+        'status', 'income_expense_type', 'category', 'subcategory'
+    ).all().order_by('-date', '-created_at')
+
+    # Извлекаем параметры фильтрации из GET-запроса
+    start_date = request.GET.get('start_date')
+    end_date = request.GET.get('end_date')
+    status_id = request.GET.get('status')
+    type_id = request.GET.get('type')
+    category_id = request.GET.get('category')
+
+    # Динамически собираем условия фильтрации через Q-объекты
+    filters = Q()
+    if start_date:
+        filters &= Q(date__gte=start_date)
+    if end_date:
+        filters &= Q(date__lte=end_date)
+    if status_id:
+        filters &= Q(status_id=status_id)
+    if type_id:
+        filters &= Q(income_expense_type_id=type_id)
+    if category_id:
+        filters &= Q(category_id=category_id)
+
+    records_list = records_list.filter(filters)
+
+    # Настраиваем пагинацию: по 5 записей на страницу
+    paginator = Paginator(records_list, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    # Обработка формы создания (POST)
     if request.method == 'POST':
         form = CashFlowRecordForm(request.POST)
         if form.is_valid():
@@ -16,11 +47,16 @@ def index(request):
             return redirect('index')
     else:
         form = CashFlowRecordForm()
-        
-    return render(request, 'core/index.html', {
-        'records': records,
-        'form': form
-    })
+
+    # Данные для отрисовки фильтров в HTML
+    context = {
+        'page_obj': page_obj,
+        'form': form,
+        'statuses': Status.objects.all(),
+        'types': Type.objects.all(),
+        'categories': Category.objects.all(),
+    }
+    return render(request, 'core/index.html', context)
 
 def get_categories(request):
     """Возвращает JSON с категориями для выбранного типа операции"""
